c72ab69fef86725e4a0937fc1214d480fb409633
Ivan <34632454+abreulima@users.noreply.github.com>
2025-07-12 11:06:48 +0100

    Create tuplas_dicts_e_listas.py

---
+++ tuplas_dicts_e_listas.py
@@ -0,0 +1,12 @@
+# Uma tupla (tuple) composto por dois inteiros
+ponto = (50, 75)
+
+# Uma lista (list) com tipos variados
+pokemon = ["Pikachu", 100]
+
+# Um dicionário (dict)
+carro = {
+  "modelo": "Avantgard",
+  "fabricante": "Mercedes",
+  "ano": 2025
+}
