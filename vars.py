d10eb177f671e421131ba5fd1d82fb4a861f8b17
Ivan <34632454+abreulima@users.noreply.github.com>
2025-07-12 10:46:08 +0100

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,16 @@
+# Tipo numérico inteiro
+idade = 5
+
+# Tipo numérico decimal 
+pontos = 6.75
+
+# Tipo textual
+nome = "Assembly"
+
+# Tipo booleano
+a_chover = False
+
+print(idade)
+print(pontos)
+print(nome)
+print(a_chover)
